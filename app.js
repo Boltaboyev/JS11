0d71742085eef6edbc87670d7e3e98c7b4541080
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-10 11:08:18 +0500

    js

---
+++ app.js
@@ -0,0 +1,31 @@
+// 2 ------------------------------------------------------
+// let printArray = (array) => array.join(",")
+// console.log(printArray(["h", "o", "l", "a"]))
+
+// 3 Find Mean------------------------------------------------------
+// let num = [1, 3, 5, 7]
+// let count = 0
+// let res = 0
+// for (let i = 0; i < num.length; i++) {
+//     count += num[i]
+// }
+// res = count / num.length
+// console.log(res)
+
+// 4 ------------------------------------------------------
+// let stringToArray = (str) => str.split(" ")
+// console.log(stringToArray("hello world"))
+
+// 5 ---------------------------------------------------------
+// function getAverage(arr) {
+//     let sum = arr.reduce((acc, num) => acc + num, 0)
+//     let average = sum / arr.length
+//     return Math.floor(average)
+// }
+// let scores = [1, 2, 2, 2, 2, 2, 1]
+// console.log(getAverage(scores))
+
+// 6 --------------------------------------------------------
+// let num = [34, -345, -1, 100];
+// let res = Math.min(...num);
+// console.log(res);
